1ff871c35e72edaf9aab13f2441e4b77085b3854
Sumit Soni <128374634+Sumit-Raghukant-Soni@users.noreply.github.com>
2024-05-19 11:24:09 +0530

    checking server 5

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-// import express from "express";
-
-// const app = express();
-// const port = 9000;
-
-// app.use("/", (req, res) => {
-//     res.json({ message : "Server is working" });
-// })
-
-// app.listen(port, () => {
-//     console.log(`Starting server on Port ${port}`);
-// });
-
-
 const express = require('express');
 const app = express();
 const { v4: uuidv4 } = require('uuid');
