809c8461b828e00f7c09bc8a9266503e6b665d7b
Sumit Soni <128374634+Sumit-Raghukant-Soni@users.noreply.github.com>
2024-05-19 11:18:35 +0530

    checking server

--- server.js
+++ server.js
@@ -1,40 +1,40 @@
 const express = require('express');
 const app = express();
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
 
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const { ExpressPeerServer } = require('peer');
-const peerServer = ExpressPeerServer(server, {
-    debug: true
-});
+// const server = require('http').Server(app);
+// const io = require('socket.io')(server);
+// const { ExpressPeerServer } = require('peer');
+// const peerServer = ExpressPeerServer(server, {
+//     debug: true
+// });
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.use('/peerjs', peerServer);
+// app.use('/peerjs', peerServer);
 
 app.get('/', (req, res) => {
     res.json({ message : "Server is working" });
     // res.redirect(`/${ uuidv4() }`);
 });
 
-app.get('/:room', (req, res) => {
-    res.render('room', { roomId: req.params.room });
-})
+// app.get('/:room', (req, res) => {
+//     res.render('room', { roomId: req.params.room });
+// })
 
 
-io.on('connection', socket => {
-    socket.on('join-room', (roomId, userId) => {
-        // console.log('joined room');
-        socket.join(roomId);
-        socket.to(roomId).emit('user-connected', userId);
-        // socket.broadcast.emit('user-connected', userId);
+// io.on('connection', socket => {
+//     socket.on('join-room', (roomId, userId) => {
+//         // console.log('joined room');
+//         socket.join(roomId);
+//         socket.to(roomId).emit('user-connected', userId);
+//         // socket.broadcast.emit('user-connected', userId);
 
-        socket.on('message', message => {
-            io.to(roomId).emit('createMessage', message);
-        })
-    })
-})
+//         socket.on('message', message => {
+//             io.to(roomId).emit('createMessage', message);
+//         })
+//     })
+// })
 
 server.listen(3030);
